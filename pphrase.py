c2505be495d3cd73e5854c0e5ed8c248b8698db1
merionum <vadim0006@gmail.com>
2019-10-16 20:18:15 +0300

    update

--- pphrase.py
+++ pphrase.py
@@ -1,14 +1,15 @@
-from spacy.symbols import *
+from spacy.symbols import nsubj, nsubjpass, dobj, iobj, pobj
 from nltk.tokenize import sent_tokenize, word_tokenize
 import spacy_udpipe
 import re
 
-# TODO move function_words to extract_phrases
 
 def handle_cyrillic_yo(tokens):
     for phrase in tokens:
         if 'ё' in phrase:
-            tokens.append(phrase.replace('ё', 'е'))
+            replaced_e = phrase.replace('ё', 'е')
+            if replaced_e not in tokens:
+                tokens.append(replaced_e)
     return tokens
 
 class Extractor:
@@ -19,24 +20,33 @@ class Extractor:
     """
     def __init__(self, udpipe_model='russian-syntagrus-ud-2.4-190531.udpipe',
                  lang='ru', np_labels=[nsubj, nsubjpass, dobj, iobj, pobj],
-                 function_words_path=None):
+                 function_words=None, derivative_prepositions=None):
 
         print('Initializing...')
         self.nlp = spacy_udpipe.load_from_path(lang, udpipe_model)
         self.np_labels = set(np_labels)
 
-        if function_words_path is not None:
-            self.check_functional = True
-            with open(function_words_path, 'r') as f:
-                self.function_words = handle_cyrillic_yo(
+        self.function_words = function_words
+        if self.function_words is not None:
+            self.context_funct = max([len(p.split()) for p in self.function_words]) - 1
+            if lang == 'ru':
+                self.function_words = self.__prep_cyr_words(self.function_words)
 
-                                      [re.sub('[^А-Яа-яЁё]+', ' ', w.strip('\n').lower()).strip() \
-                                       for w in f.readlines() if not w.endswith(':') \
-                                       and len(w.split()) > 1])
-
-                self.context_window = max([len(p.split()) for p in self.function_words]) - 1
-
+        self.derivative_prepositions = derivative_prepositions
+        if self.derivative_prepositions is not None:
+            self.context_der = max([len(p.split()) for p in self.derivative_prepositions]) - 1
+            if lang == 'ru':
+                self.function_words = self.__prep_cyr_words(self.function_words)
+            if self.function_words is not None:
+                self.derivative_prepositions = [p for p in self.derivative_prepositions
+                                                if p not in self.function_words]
         print('Ready!')
+
+    def __prep_cyr_words(self, tokens):
+        for tok_id in range(len(tokens)):
+            tokens[tok_id] = re.sub('[^а-яё]+', ' ', tokens[tok_id].lower().strip())
+        tokens = handle_cyrillic_yo(tokens)
+        return tokens
 
     def __process_text(self, text: str):
         tokens = text.replace('\n', '.').strip().lower().split()
@@ -56,21 +66,19 @@ class Extractor:
             if len(new_sent):
                 new_text.append(' '.join(new_sent))
         text = new_text
-        text = '. '.join([re.sub('[^А-Яа-яЁёA-Za-z]+', ' ', sent).strip() for sent in text])
+        text = '. '.join([re.sub('[^а-яёa-z]+', ' ', sent).strip() for sent in text])
+        # unnecessary (remove)
         for gar in ['n ст.', ' n фз']:
             text = text.replace(gar, '')
         return text
 
-    def __iter_nps(self, doc):
-        # TODO
-        for word in doc:
-            if word.dep in self.np_labels:
-                yield word.subtree
+    def __get_context(self, token, window):
+        return self.doc[token.i-window-1:token.i+window]
 
     def __is_part_functional(self, token):
         # check if preposition is a part of any of functional words in
-        # context_window range
-        context = self.doc[token.i-self.context_window:token.i+self.context_window].text
+        # context_funct range
+        context = self.__get_context(token, self.context_funct).text
         if len(context.split('.')) > 1:
             context = context.split('.')[1]
         candidates = list(filter(lambda x: x if x in context else None, self.function_words))
@@ -80,7 +88,19 @@ class Extractor:
         return False
 
     def __is_part_derivative(self, token):
-        return
+        context = self.__get_context(token, self.context_der)
+        found_derivative = list(filter(lambda x: set(x.split()).issubset(context.text.split()),
+                                                 self.derivative_prepositions))
+        if found_derivative:
+            self.deriv_context = context
+            self.found_derivative = max(found_derivative, key=len).split()
+            return True
+        return False
+    
+    def __add_phrase_to_output(self, preposition, phrase):
+        if preposition not in self.prep_phrases:
+            self.prep_phrases[preposition] = list()
+        self.prep_phrases[preposition].append(phrase)
 
     def extract_phrases(self, text: str):
         self.doc = self.nlp(self.__process_text(text))
@@ -88,23 +108,36 @@ class Extractor:
         for token in self.doc:
             if token.pos_ == 'ADP':
 
-                if self.check_functional:
+                if self.function_words is not None:
                     if self.__is_part_functional(token):
                         continue
 
-                if self.__is_part_derivative(token):
-                    pass
+                if self.derivative_prepositions is not None:
+                    if self.__is_part_derivative(token):
+                        # TODO: move to new function (__extract_derivative_from_context)
+                        orig_con_idx = [self.deriv_context[0].i, self.deriv_context[-1].i]
+                        while self.deriv_context[orig_con_idx[0]].text != self.found_derivative[0]:
+                            orig_con_idx[0] += 1
+                        print()
+                        while self.deriv_context[orig_con_idx[1]].text != self.found_derivative[-1]:
+                            orig_con_idx[1] -= 1
+                        derivative = self.deriv_context[orig_con_idx[0]:orig_con_idx[1]+1]
+                        if derivative[-1].pos_ != 'ADP':
+                            slave_i = derivative[-1].i + 1
+                            while self.doc[slave_i].head != derivative[-1]:
+                                slave_i += 1
+                            slave = self.doc[slave_i]
+                            extracted_phrase = ' '.join([derivative[-1].head.text,
+                                                         derivative.text,
+                                                         slave.text])
+                        else:
+                            extracted_phrase = ' '.join([derivative[-1].head.head.text, 
+                                                         derivative.text,
+                                                         derivative[-1].head.text])
+                        preposition = derivative.text
+                        self.__add_phrase_to_output(preposition, extracted_phrase)
+                        continue
 
-                # constr_idx = [token.head.head.i, token.i, token.head.i]
-
-                # if token.text not in self.prep_phrases:
-                #     self.prep_phrases[token.text] = list()
-                # constr_idx = [token.head.head.i, token.i, token.head.i]
-
-                # # get nps
-                # context = self.doc[min(constr_idx):max(constr_idx)+1].text
-
-                # if not self.__is_part_extracted(extracted_context):
-                #     self.prep_phrases[token.text].append(extracted_context)
-        return 'Okay'
-        # self.prep_phrases
+                extracted_phrase = ' '.join([token.head.head.text, token.text, token.head.text])
+                self.__add_phrase_to_output(token, extracted_phrase)
+        return self.prep_phrases
